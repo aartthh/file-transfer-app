f08b15d5861fe7c41e6615228b78eeae07ab8842
aartthh <22ucs035@lnmiit.ac.in>
2025-07-18 03:16:01 +0530

    Describe your changes here

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -10,48 +9,22 @@ const userSchema = new mongoose.Schema({
     minlength: 3,
     maxlength: 20
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true
-  },
   password: {
     type: String,
     required: true,
     minlength: 6
   },
-  isOnline: {
-    type: Boolean,
-    default: false
-  },
-  lastSeen: {
+  createdAt: {
     type: Date,
     default: Date.now
   },
-  socketId: {
-    type: String,
-    default: null
-  }
-}, {
-  timestamps: true
-});
-
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
+  lastLogin: {
+    type: Date,
+    default: Date.now
   }
 });
 
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
+// Index for faster queries
+userSchema.index({ username: 1 });
 
 module.exports = mongoose.model('User', userSchema);
